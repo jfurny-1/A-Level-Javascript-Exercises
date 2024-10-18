53620e82c4b35ec4dbfc9a5c5526d6cbf2ce8784
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-10-18 12:57:35 +0100

    works

--- My Work:/script.js
+++ My Work:/script.js
@@ -8,32 +8,33 @@ let image2 = document.getElementById('door2')
 let image3 = document.getElementById('door3')
 
 function door1Clicked(){
-    alert(doorMan)
-     door1Click = True;
     
-    if (doorMan == 1) {
+     door1Click = true;
+    
+    if (doorMan === 1) {
         image1.src="man.jpg"
+        alert(doorMan)
         
     }
 
     };
 
     function door2Clicked(){
-        alert(doorMan)
-         door2Click = True
-        if (doorMan == 2) {
+        
+         door2Click = true
+        if (doorMan === 2) {
             image2.src="man.jpg"
-            
+            alert(doorMan)
         }
         
     };
 
     function door3Clicked(){
-        alert(doorMan)
-         door3Click = True
-        if (doorMan == 3) {
+        
+         door3Click = true
+        if (doorMan === 3) {
             image3.src="man.jpg"
-            
+            alert(doorMan)
         }
         
     };
