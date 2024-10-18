209fb5b61e1a71642e35f29afd1106af57b9eea7
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-10-18 12:39:55 +0100

    all doors but not working image change

--- My Work:/script.js
+++ My Work:/script.js
@@ -1,15 +1,41 @@
 
 let doorMan = Math.floor(Math.random() * 4);
+let door1Click = false;
+let door2Click = false;
+let door3Click = false;
+let image1 = document.getElementById('door1')
+let image2 = document.getElementById('door2')
+let image3 = document.getElementById('door3')
 
-function door1() {
+function door1Clicked(){
     
-    let door1Click = True;
+     door1Click = True;
 
-    if (door1Click = True && doorMan.value = 1) {
-        door2.innerHTML=""
+    if (doorMan.value === 1) {
+        image1.src="OIP (1).jpg"
     }
 
-    }
+    };
+
+    function door2Clicked(){
+
+         door2Click = True
+        if (doorMan.value === 2) {
+            image2.src="OIP (1).jpg"
+        }
+        
+    };
+
+    function door3Clicked(){
+
+         door3Click = True
+        if (doorMan.value === 3) {
+            image3.src="OIP (1).jpg"
+        }
+        
+    };
+
+
 
 
 
