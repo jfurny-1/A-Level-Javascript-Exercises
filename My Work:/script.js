4c7f7ef3a47ee7ba64fa737be4b8fcb727bf21e6
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-10-18 12:19:25 +0100

    3 buttons + rng

---
+++ My Work:/script.js
@@ -0,0 +1,15 @@
+
+let doorMan = Math.floor(Math.random() * 4);
+
+function door1() {
+    
+    let door1Click = True;
+
+    if (door1Click = True && doorMan.value = 1) {
+        door2.innerHTML=""
+    }
+
+    }
+
+
+
