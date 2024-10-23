11a3efa481b66ebc1813531095e99601c83289e3
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-10-23 15:27:26 +0100

    cami

--- My Work:/naughts and crosses/script.js
+++ My Work:/naughts and crosses/script.js
@@ -1,62 +1,61 @@
-
+let counter = 0
 function square(num){
-    if (num === 1) {
-        button1.src = "x"
+    if (counter % 2 === 0) {
+        if (num === 1) {
+            button1.src = "x"
+        }
+        else if (num === 2) {
+            button1.src = "x"
+        }
+        else if (num === 3) {
+            button1.src = "x"
+        }
+        else if (num === 4) {
+            button1.src = "x"
+        }
+        else if (num === 5) {
+            button1.src = "x"
+        }
+        else if (num === 6) {
+            button1.src = "x"
+        }
+        else if (num === 7) {
+            button1.src = "x"
+        }
+        else if (num === 8) {
+            button1.src = "x"
+        }
+        else if (num === 9) {
+            button1.src = "x"
+        }
+    }   else {
+        if (num === 1) {
+            button1.src = "o"
+        }
+        else if (num === 2) {
+            button1.src = "o"
+        }
+        else if (num === 3) {
+            button1.src = "o"
+        }
+        else if (num === 4) {
+            button1.src = "o"
+        }
+        else if (num === 5) {
+            button1.src = "o"
+        }
+        else if (num === 6) {
+            button1.src = "o"
+        }
+        else if (num === 7) {
+            button1.src = "o"
+        }
+        else if (num === 8) {
+            button1.src = "o"
+        }
+        else if (num === 9) {
+            button1.src = "o"
+        }
     }
-    else if (num === 2) {
-        button1.src = "x"
-    }
-    else if (num === 3) {
-        button1.src = "x"
-    }
-    else if (num === 4) {
-        button1.src = "x"
-    }
-    else if (num === 5) {
-        button1.src = "x"
-    }
-    else if (num === 6) {
-        button1.src = "x"
-    }
-    else if (num === 7) {
-        button1.src = "x"
-    }
-    else if (num === 8) {
-        button1.src = "x"
-    }
-    else if (num === 9) {
-        button1.src = "x"
-    }
-
-}
-
-function square(num){
-    if (num === 1) {
-        button1.src = "o"
-    }
-    else if (num === 2) {
-        button1.src = "o"
-    }
-    else if (num === 3) {
-        button1.src = "o"
-    }
-    else if (num === 4) {
-        button1.src = "o"
-    }
-    else if (num === 5) {
-        button1.src = "o"
-    }
-    else if (num === 6) {
-        button1.src = "o"
-    }
-    else if (num === 7) {
-        button1.src = "o"
-    }
-    else if (num === 8) {
-        button1.src = "o"
-    }
-    else if (num === 9) {
-        button1.src = "o"
-    }
-
+    counter++
 }
