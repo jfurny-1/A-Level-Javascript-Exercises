625582039e0e3466f0b26f0f1014e0d897aceb3d
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-10-23 15:24:06 +0100

    2 functions

--- My Work:/naughts and crosses/script.js
+++ My Work:/naughts and crosses/script.js
@@ -3,4 +3,60 @@ function square(num){
     if (num === 1) {
         button1.src = "x"
     }
+    else if (num === 2) {
+        button1.src = "x"
+    }
+    else if (num === 3) {
+        button1.src = "x"
+    }
+    else if (num === 4) {
+        button1.src = "x"
+    }
+    else if (num === 5) {
+        button1.src = "x"
+    }
+    else if (num === 6) {
+        button1.src = "x"
+    }
+    else if (num === 7) {
+        button1.src = "x"
+    }
+    else if (num === 8) {
+        button1.src = "x"
+    }
+    else if (num === 9) {
+        button1.src = "x"
+    }
+
+}
+
+function square(num){
+    if (num === 1) {
+        button1.src = "o"
+    }
+    else if (num === 2) {
+        button1.src = "o"
+    }
+    else if (num === 3) {
+        button1.src = "o"
+    }
+    else if (num === 4) {
+        button1.src = "o"
+    }
+    else if (num === 5) {
+        button1.src = "o"
+    }
+    else if (num === 6) {
+        button1.src = "o"
+    }
+    else if (num === 7) {
+        button1.src = "o"
+    }
+    else if (num === 8) {
+        button1.src = "o"
+    }
+    else if (num === 9) {
+        button1.src = "o"
+    }
+
 }
