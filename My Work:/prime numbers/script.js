44f20f4e55d04cc96dd0755c530be9111f2cffea
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-11-18 10:49:21 +0000

    bhj

--- My Work:/prime numbers/script.js
+++ My Work:/prime numbers/script.js
@@ -2,28 +2,21 @@
 
 
 function primeCalc() {
-output.innerHTML = ""
 let minNum = document.getElementById('minNum').value;
 let maxNum = document.getElementById('maxNum').value;
 let range = Number(maxNum) - Number(minNum);
-    let primeVerifier = false;
+
     
    
     for (let i = 0; i <= Number(range); i++) {
-        
         for(let l = Number(minNum); l <= Number(maxNum); l++) {
-            
             if(l % i === 0) {
-            primeVerifier = false;
-
         } else {
-            
-            primeVerifier = true;   
             let value = output.innerHTML;
             alert(value)
-         output.innerHTML = output.innerHTML + `${l} <br>`;
+         output.innerHTML = output.innerHTML + `${Number(l)} <br>`;
 
         }
-    }
+      }
     }
 }
