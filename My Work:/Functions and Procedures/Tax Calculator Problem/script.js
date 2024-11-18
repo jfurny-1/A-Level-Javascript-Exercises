8edd4497c7988d0e2e457d14749eeb2c7d4d50d2
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-11-18 10:45:08 +0000

    www

--- My Work:/Functions and Procedures/Tax Calculator Problem/script.js
+++ My Work:/Functions and Procedures/Tax Calculator Problem/script.js
@@ -1,16 +1,33 @@
 // taxCalculator.js
 
 // Function to calculate tax based on income
-function ________________________() {
+function calculateTax(annualSalary, personalAllowance) {
     // TODO: Calculate taxable income (Annual Salary - Personal Allowance)
+    let annualSalary = document.getElementById("annualSalary").value
+    let personalAllowance = document.getElementById("personalAllowance").value
+let taxableIncome = (annualSalary - personalAllowance);
     // TODO: Use tax bands and rates to calculate total tax
+    let totalTax = 0
+    if (taxableIncome < 12570) {
+        totalTax === 0
+    } else if (taxableIncome > 12570 && taxableIncome < 14877){
+        totalTax = taxableIncome * 0.19
+    } else if (taxableIncome > 14876 && taxableIncome < 26563){
+        totalTax = taxableIncome * 0.2
+    } else if (taxableIncome > 27562 && taxableIncome < 43663){
+        totalTax = taxableIncome * 0.21
+    } else if (taxableIncome > 43662 && taxableIncome < 75001){
+        totalTax = taxableIncome * 0.42
+    } else if (taxableIncome > 75000 && taxableIncome < 125141){
+        totalTaxe = taxableIncome * 0.45
+    } else (taxableIncome > 125140);{
+        totalTax = taxableIncome * 0.48
+    }
+    outputTax.innerHTML = `The total tax is ${totalTax}`
 }
 
 // Event listener for the button
-document.getElementById("btnCalculateTax").addEventListener("click", () => {
-    const annualSalary = parseFloat(document.getElementById("annualSalary").value);
-    const personalAllowance = parseFloat(document.getElementById("personalAllowance").value);
     
     // TODO: Validate inputs and call the tax calculation function
     // TODO: Display the result in "taxResult" div
-});
+
