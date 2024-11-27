35598a912933318b70fe9325c1a94cc739cca033
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-11-27 15:19:00 +0000

    wew

--- My Work:/Rock, Paper, Scissors/script.js
+++ My Work:/Rock, Paper, Scissors/script.js
@@ -1,1 +1,36 @@
-document.body.style.backgroundColor = "#AA0000"
+document.body.style.backgroundColor = "#AA0000";
+
+function startGame(){
+
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        rotatingImage.src="paper.jpg"
+    } else if (choice === 2) { 
+        rotatingImage.src="R.png"
+    } else {
+        rotatingImage.src="rock.jpg"
+    }
+
+    let choice2 = Math.floor(Math.random() * 3) + 1;
+    if (choice2 === 1) {
+        rotatingImage.src="paper.jpg"
+    } else if (choice2 === 2) { 
+        rotatingImage.src="R.png"
+    } else {
+        rotatingImage.src="rock.jpg"
+    }
+
+    if (choice === 1 && choice2 === 1) { 
+        winnerOutput.innerHTML = `DRAW!`
+    } else if (choice === 2 && choice2 === 2) { 
+        winnerOutput.innerHTML = `DRAW!`
+    } else if (choice === 3 && choice2 === 3) { 
+        winnerOutput.innerHTML = `DRAW!`
+    } 
+
+
+
+
+}
+
+
