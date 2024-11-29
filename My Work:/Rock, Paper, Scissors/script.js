46c6a577a8b58d32f7125f605a028b9c1c5f5ef7
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-11-29 12:58:21 +0000

    hyu

--- My Work:/Rock, Paper, Scissors/script.js
+++ My Work:/Rock, Paper, Scissors/script.js
@@ -5,9 +5,25 @@ function startGame(){
     let choice1 = document.getElementById('player1')
     let choice2 = document.getElementById('player2')
 
+    if (choice1 === "rock") {
+        player1Choice.src = "rock.jpg"
+    } else if (choice1 === "paper") {
+        player1Choice.src = "paper.png" 
+    } else if (choice1 === "scissors") {
+        player1Choice.src = "scissors.jpg"
+    }
+
+    if (choice2 === "rock") {
+        player2Choice.src = "rock.jpg"
+    }  else if (choice2 === "paper") {
+        player2Choice.src = "paper.png" 
+    } else if (choice2 === "scissors") {
+        player2Choice.src ="scissors.jpg"
+    }
+
+
     if (choice1 === choice2) {
         winnerOutput.innerHTML = `DRAW!`
-            
     } else if (choice1 === "rock" && choice2 === "paper") {
         winnerOutput.innerHTML = `Player 2 WINS!`
     } else if(choice1 === "rock" && choice2 === "scissors") {
