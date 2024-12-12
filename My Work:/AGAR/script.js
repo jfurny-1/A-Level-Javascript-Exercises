cb8a521ce24191aff069be16c4d0377d64948f51
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-12-12 10:37:57 +0000

    w

--- My Work:/AGAR/script.js
+++ My Work:/AGAR/script.js
@@ -1,5 +1,30 @@
+
+let player1;
+
+
+
 function setup() {
 	new Canvas(785, 400);
+	
+	player1 = new Sprite(); 
+	player1.diameter = 50;
+}
+
+function movement() {
+	if (kb.pressing('a')) 
+player1.vel.x = -5;
+	else if (kb.pressing('d'))
+player1.vel.x = 5;
+	else 
+player1.vel.x = 0;
+
+    if (kb.pressing('w'))
+player1.vel.y = 5;
+    else if (kb.pressing('s'))
+player1.vel.y = -5;
+	else
+player1.vel.y = 0;
+
 }
 
 function draw() {
