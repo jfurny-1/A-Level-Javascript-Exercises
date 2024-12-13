6b710f0b4aaa2fc6aa529b403abb2d0062273a73
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2024-12-13 13:29:13 +0000

    pom

--- My Work:/PONG/script.js
+++ My Work:/PONG/script.js
@@ -20,33 +20,33 @@ function setup() {
     stopper1 = new Sprite();
     stopper1.height = 5;
     stopper1.width = 10;
-    stopper1.x = 10
-    stopper1.y = windowHeight
-    stopper1.collider = 'static'
+    stopper1.x = 10;
+    stopper1.y = windowHeight;
+    stopper1.collider = 'static';
 
     stopper2 = new Sprite();
     stopper2.height = 5;
     stopper2.width = 10;
-    stopper2.x = 10
-    stopper2.y = -windowHeight
-    stopper2.collider = 'static'
+    stopper2.x = 10;
+    stopper2.y = 0;
+    stopper2.collider = 'static';
 
     stopper3 = new Sprite();
     stopper3.height = 5;
     stopper3.width = 10;
-    stopper3.x = 1540
-    stopper3.y = windowHeight
-    stopper3.collider = 'static'
+    stopper3.x = 1540;
+    
+    stopper3.collider = 'static';
 
     stopper4 = new Sprite();
     stopper4.height = 5;
     stopper4.width = 10;
-    stopper4.x = 1540
-    stopper4.y = -windowHeight
-    stopper4.collider = 'static'
+    stopper4.x = 1500;
+    stopper4.y = 0
+    stopper4.collider = 'static';
 
     player1.x = 10;
-    player2.x = 1540;
+    player2.x = windowWidth -10;
 
 
 }
@@ -55,9 +55,9 @@ function draw() {
 
     background('black');
 
+    ball.color = ('white');
     player1.color = ('white');
     player2.color = ('white');
-    ball.color = ('white');
     stopper1.color = ('white');
     stopper2.color = ('white');
     stopper3.color = ('white');
