78cd75b6659462f94a201295795d0807c01a22e0
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-01-15 15:27:48 +0000

    ww

--- My Work:/MK/script.js
+++ My Work:/MK/script.js
@@ -1,11 +1,42 @@
 let player1, player2;
 function preload() {
-    cage = loadImage('cage-background.jpg')
+    cage = loadImage('cage-background.jpg');
+    
 }
 
 function setup() {
+
+    // Canvas
     new Canvas(windowWidth, windowHeight)
+    
+    // Sprites
+    player1 = new Sprite();
+    
+
+    //Sprite Images
+    player1.img = 'SpaceMan.png'
+    player1.img.scale = 0.3
+
+    //Spawns
+    player1.x = 100
+    player1.y = 530
+
+
+
 }
 function draw(){
+    clear();
     image(cage, 0, 0, windowWidth, windowHeight)
+
+    //Player Speed
+    player1.speed = 10;
+
+    //Player1 Movement
+    if(kb.pressing('w')) {
+        player1.direction = 0;
+    } else if(kb.pressing('s')) {
+        player1.direction = 180;
+    } else {
+        player1.speed = 0;
+    }
 }
