78cfdf8bcef05157a7787683c516a7ce79ac2a9c
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-01-18 15:52:09 +0000

    w

--- My Work:/MK/script.js
+++ My Work:/MK/script.js
@@ -41,54 +41,8 @@ function draw(){
         player1.speed = 0;
     }
 
-    walking()
-}
-//Player 1 Walking Animation
-function walking() {
-
-    while (kb.pressing('w')) {
-
-        for(let i = 1; i > 11; i++){
-         
-         if(i = 1){
-            player1.img = 'SpaceManWalk1.png'
-            player1.img.scale = 0.3;
-         } else if(i = 2){
-            player1.img = 'SpaceManWalk2.png'
-            player1.img.scale = 0.3;
-         } else if(i = 3){
-            player1.img = 'SpaceManWalk3.png'
-            player1.img.scale = 0.3;
-         } else if(i = 4){
-            player1.img = 'SpaceManWalk4.png'
-            player1.img.scale = 0.3;
-         } else if(i = 5){
-            player1.img = 'SpaceManWalk5.png'
-            player1.img.scale = 0.3;
-         } else if(i = 6){
-            player1.img = 'SpaceManWalk6.png'
-            player1.img.scale = 0.3;
-         } else if(i = 7){
-            player1.img = 'SpaceManWalk7.png'
-            player1.img.scale = 0.3;
-         } else if(i = 8){
-            player1.img = 'SpaceManWalk8.png'
-            player1.img.scale = 0.3;
-         } else if(i = 9){
-            player1.img = 'SpaceManWalk9.png'
-            player1.img.scale = 0.3;
-         } else if(i = 10){
-            player1.img = 'SpaceManWalk10.png'
-            player1.img.scale = 0.3;
-         } else if(i = 11){
-            player1.img = 'SpaceManWalk11.png'
-            player1.img.scale = 0.3;
-            i = 1;
-         } else {
-            player1.img = 'SpaceMan.png'
-            player1.img.scale = 0.3;
-         }
-        }
-        
+    if(kb.holding('w')) {
+        player1.img = 'SpaceManWalk1.png';
+        player1.img.scale = 0.3;
     }
 }
