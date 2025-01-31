b70a11e2d9830b4f4f2dff024920dc4884210820
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-01-31 10:26:37 +0000

    w

--- My Work:/AGAR/script.js
+++ My Work:/AGAR/script.js
@@ -1,42 +1,5 @@
+document.body.style.backgroundColor = "black";
 
-let player1;
+ 
 
-function setup() {
-	
-	new Canvas(windowWidth, windowHeight);
-	
-	player1 = new Sprite(); 
-	player1.diameter = 50;
 
-}
-
-function preload() {
-
-	player1 = loadImage('New Piskel.gif');
-}
-
-function movement() {
-
-	if (kb.pressing('a')) 
-player1.vel.x = -5;
-	else if (kb.pressing('d'))
-player1.vel.x = 5;
-	else 
-player1.vel.x = 0;
-
-    if (kb.pressing('w'))
-player1.vel.y = 5;
-    else if (kb.pressing('s'))
-player1.vel.y = -5;
-	else
-player1.vel.y = 0;
-
-}
-
-function draw() {
-
-	background('blue');
-	
-	Image(player1, 0,0);
-
-}
