97796ea2bec34f19b17e10d4b062ce20643a3d71
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-02-11 15:18:20 +0000

    3wqd

--- My Work:/egypt fighter/script.js
+++ My Work:/egypt fighter/script.js
@@ -1,4 +1,5 @@
-
+let player1;
+let anubisBoss;
 
 function preload() {
     desert = loadImage('desertBackground.png')
@@ -8,9 +9,27 @@ function setup(){
 // Canvas
 new Canvas(windowWidth, windowHeight);
 
+// Charaters
+player1 = new Sprite();
+anubisBoss = new Sprite();
+
 }
 
 function draw(){
-    clear();
-    image(desert, 0, 0, windowWidth, windowHeight);
+// Background 
+clear();
+image(desert, 0, 0, windowWidth, windowHeight);
+
+// Movement
+if(kb.pressing('d')) {
+    player1.direction = 0;
+    player1.speed = 4;
+} else if(kb.pressing('a')) {
+    player1.direction = 180;
+    player1.speed = 4;
+} else {
+    player1.speed = 0;
+}
+
+
 }
