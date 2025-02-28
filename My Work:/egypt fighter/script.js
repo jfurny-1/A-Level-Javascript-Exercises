d1afcd9b2a8703c305e504832e4882d9d5935939
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-02-28 12:59:58 +0000

    ww

--- My Work:/egypt fighter/script.js
+++ My Work:/egypt fighter/script.js
@@ -2,7 +2,14 @@ let player1;
 let sandBoss;
 
 function preload() {
-    desert = loadImage('desertBackground.png')
+desert = loadImage('desertBackground.png')
+
+// Walking Animation
+walkingP1 = loadAni('player1Walk1.png',
+'player1.png',
+'player1Walk2.png',
+'player1.png', );
+walkingP1.frameDelay = 3;
 }
 
 function setup(){
@@ -12,7 +19,7 @@ new Canvas(windowWidth, windowHeight);
 // Charaters
 player1 = new Sprite();
 player1.image = 'player1.png';
-player1.image.scale = 1.1;
+player1.image.scale = 0.7;
 player1.x = 125;
 player1.y = 470;
 
@@ -21,6 +28,7 @@ sandBoss.image = 'sandBoss.png';
 sandBoss.image.scale = 0.8;
 sandBoss.x = 1430;
 sandBoss.y = 435;
+
 
 }
 
@@ -34,6 +42,7 @@ if(kb.pressing('d')) {
     player1.direction = 0;
     player1.speed = 4;
 } else if(kb.pressing('a')) {
+    player1.addAni(walkingP1)
     player1.direction = 180;
     player1.speed = 4;
 } else {
@@ -42,3 +51,4 @@ if(kb.pressing('d')) {
 
 
 }
+
