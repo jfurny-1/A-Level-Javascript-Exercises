3fbf813516c4bd0b2b02ba4a665ccf5d1ab32358
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-05-09 12:43:36 +0100

    u

--- Class Exercises/8 - Sorting Algorithms/Insertion Sort/Insertion Sort Assess/script.js
+++ Class Exercises/8 - Sorting Algorithms/Insertion Sort/Insertion Sort Assess/script.js
@@ -29,7 +29,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     function insertionSort(array) {
-  
+        for (let i = 1; i < array.length; i++) {
+            let current = array[i];
+            let j = i - 1;
+
+            while (j >= 0 && array[i] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = current
+        }
+        return array;
+    }
+
+    function insetionSort2(array) {
+        for(let i = 1; i < array.length; i++) {
+            let current = array[i];
+            let j = i - 1;
+            
+            while (j >= 0 && array[i] > current) {
+                array[j - 1] = j;
+                j--;
+            }
+            array[j + 1] = current;
+        }
+        return array;
     }
     
 
