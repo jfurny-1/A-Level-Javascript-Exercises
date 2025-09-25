e4ab02624e485ce2c4a547292a98984660ddcee8
jfurny-1 <jfurnival624@canon-slade.bolton.sch.uk>
2025-09-25 15:10:44 +0100

    Update script.js

--- Class Exercises/10 - Object Oriented Programming/Task 1a - Practice Basic Class/script.js
+++ Class Exercises/10 - Object Oriented Programming/Task 1a - Practice Basic Class/script.js
@@ -1,16 +1,16 @@
 // TODO: Create a Book class with a constructor that takes title and author
 // Add a method getInfo() that returns a string like: "Title by Author"
 
-// Uncomment and complete below:
-// class Book {
-//   constructor(title, author) {
-//     // your code here
-//   }
-//   getInfo() {
-//     // your code here
-//   }
-// }
-
+ Uncomment and complete below:
+ class Book {
+   constructor(title, author) {
+     this.title = title;
+     this.author = author;
+   }
+   getInfo() {
+     return `${title} by ${author}`
+   }
+}
 // Example usage (leave this):
 document.getElementById("btnShow").onclick = function() {
   // const book = new Book("1984", "George Orwell");
